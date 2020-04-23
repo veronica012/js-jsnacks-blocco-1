a59fc58c1fbfd4c46616dc40acf7a56c0b6b2d9f
Veronica Celseti <veronicacelseti12@gmail.com>
2020-04-23 14:41:18 +0200

    array insieme dei numeri elevati al cubo

--- jsnack5/main.js
+++ jsnack5/main.js
@@ -1,7 +1,12 @@
 //cubo dei primi n numeri dove n è inserito dall'utente
+//array vuoto
+var insieme_cubi = [];
+//chiedo un numero all'utente
 var numero = parseInt(prompt('Inserisci un numero'));
-//console.log(numero);
+//calcolo il cubo dei numeri da 0 a n, n=numero inserito dall'utente
 for(var i = 0; i < numero ; i++ ) {
     var cubo = Math.pow(i, 3);
     console.log(cubo);
+    insieme_cubi.push(cubo);
 }
+console.log(insieme_cubi);
